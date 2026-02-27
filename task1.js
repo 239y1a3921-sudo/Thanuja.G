fbb211285a80302f3a5ab72ceaafe41487a82c16
239y1a3921-sudo <239y1a3921@ksrmce.ac.in>
2026-02-27 15:23:42 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,4 @@
+let value = 42;        // value is a number
+value = "forty-two";   // now value is a string
+
+console.log(value);
---
+++ task3.js
@@ -0,0 +1,3 @@
+let total = 100;
+total = "100";
+console.log(total + 20); // "10020" (bug, but no error)
